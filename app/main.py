f91b21d46524577fa173b26f90e7e4544f340e36
Eric McLean <eric.mclean@infermedica.com>
2025-04-18 11:32:48 -0400

    for realz now, I have all the requirements

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # main.py
-
+from fhirclient.models.bundle import Bundle, BundleEntry
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from .db import models
@@ -19,6 +19,28 @@ def get_db():
         db.close()
 
 
+@app.get("/fhir/Patients")
+def get_patients(db: Session = Depends(get_db)):
+    patients_db = db.query(models.Patient).all()
+    if not patients_db:
+        return {"error": "No patients found"}
+
+    # create a FHIR Bundle
+    bundle = Bundle()
+    bundle.type = "searchset"
+    bundle.entry = []
+
+    for p in patients_db:
+        fhir_patient = fhir_mapper.db_patient_to_fhir(p)
+
+        entry = BundleEntry()
+        entry.resource = fhir_patient
+        entry.fullUrl = f"Patient/{fhir_patient.id}"
+        bundle.entry.append(entry)
+
+    return bundle.as_json()
+
+
 @app.get("/fhir/Patient/{patient_id}")
 def get_patient(patient_id: int, db: Session = Depends(get_db)):
     patient_db = (
@@ -27,6 +49,40 @@ def get_patient(patient_id: int, db: Session = Depends(get_db)):
     if not patient_db:
         return {"error": "Patient not found"}
     return fhir_mapper.db_patient_to_fhir(patient_db).as_json()
+
+
+@app.get("/fhir/Observations")
+def get_observations(db: Session = Depends(get_db)):
+    heart_rates_db = db.query(models.HeartRate).all()
+    if not heart_rates_db:
+        return {"error": "No heart rate observations found"}
+
+    # create a FHIR Bundle for both hr and bp
+    bundle = Bundle()
+    bundle.type = "searchset"
+    bundle.entry = []
+
+    for hr in heart_rates_db:
+        fhir_heartrate = fhir_mapper.heart_rate_to_observation(hr)
+
+        entry = BundleEntry()
+        entry.resource = fhir_heartrate
+        entry.fullUrl = f"Observation/{hr.id}"
+        bundle.entry.append(entry)
+
+    blood_pressures_db = db.query(models.BloodPressure).all()
+    if not blood_pressures_db:
+        return {"error": "No blood pressure observations found"}
+
+    for bp in blood_pressures_db:
+        fhir_bloodpressure = fhir_mapper.blood_pressure_to_observation(bp)
+
+        entry = BundleEntry()
+        entry.resource = fhir_bloodpressure
+        entry.fullUrl = f"Observation/{bp.id}"
+        bundle.entry.append(entry)
+
+    return bundle.as_json()
 
 
 @app.get("/fhir/Observation/heart_rate/{patient_id}")
